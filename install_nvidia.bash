419b78a9062a09c2445545987430da4b54bc88f3
kirillin <kaartemov@gmail.com>
2020-01-27 20:51:57 +0300

    Repairs Dockerfile for virtualbox

--- install_nvidia.bash
+++ install_nvidia.bash
@@ -2,8 +2,8 @@
 
 echo "Some preparations..."
 # cd ~ && git clone https://github.com/be2rlab/yaprofi_robotics_2020.git
-mkdir -p catkin_ws/src && cd catkin_ws/src
-cd ~/yaprofi_robotics_2020 && mv ./*task ./
+cd ~ && mkdir -p catkin_ws/src && cd catkin_ws/src
+mv ~/yaprofi_robotics_2020/*task ./
 
 echo "Installing docker dependencies"
 cd ./docker/nvidia && sudo bash install_docker.bash
